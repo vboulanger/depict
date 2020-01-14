2549e6d81f4c040bee891b91d2a989fbd0f483d7
Victor <vb@live.fr>
2020-01-14 22:58:56 +0100

    Minor

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="depict",
-    version="0.1.2",
+    version="0.1.3",
     author="Victor Boulanger",
     author_email="vb@live.fr",
     description="Business grade visualizations in seconds",
