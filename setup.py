8ca0dc7021720b465909da2cefc2df3612877697
vboulanger <victor.boulanger@epfl.ch>
2020-11-17 12:57:48 +0100

    Minor update requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="depict",
-    version="1.0.2",
+    version="1.0.4",
     author="Victor Boulanger",
     author_email="vb@live.fr",
     description="Business grade visualizations in seconds",
@@ -22,7 +22,7 @@ setuptools.setup(
         "Topic :: Scientific/Engineering :: Visualization",
     ],
     python_requires='>=3.5',
-    install_requires=['bokeh>=1.4.0', 'seaborn>=0.9.0', 'pandas>=0.24.2'],
+    install_requires=['bokeh>=2.0.0', 'seaborn>=0.9.0', 'pandas>=0.24.2'],
     extras_require={'test': ['pytest', 'pycodestyle'],
                     'doc': ['Sphinx']},
     tests_require=['pytest'],
